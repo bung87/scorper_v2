6972395180d71bb1960b47ba6b38eacbe6fcc6e4
bung87 <crc32@qq.com>
2024-01-13 21:26:23 +0800

    mime

--- examples/accept_mime.nim
+++ examples/accept_mime.nim
@@ -1,18 +1,35 @@
 import scorper_v2
+import macros
 
 const port{.intdefine.} = 8080
 
 when isMainModule:
-  proc cb() {.gcsafe, nimcall, raises: [].} =
+  proc cb() =
+    var matched = false
     for mime in acceptMimes():
       case mime
       of "html":
         let headers = {"Content-Type": "text/html"}
+        matched = true
         reply("Hello, World!", headers)
         break
+    if not matched:
+      let headers = {"Content-Type": "text/html"}
+      reply("default response", headers)
+
+  proc cb2() =
+    acceptMime:
+      case ext
+      of "html":
+        let headers = {"Content-Type": "text/html"}
+        reply("Hello, World!", headers)
       else:
-        discard
+        let headers = {"Content-Type": "text/html"}
+        reply("default response", headers)
 
   let server = newHttpServer(cb)
   server.start(port)
-  joinThreads(server.thread)
+  
+  let server2 = newHttpServer(cb2)
+  server2.start(8081)
+  joinThreads( server2.thread)
--- src/scorper_v2/accept_mime.nim
+++ src/scorper_v2/accept_mime.nim
@@ -1,4 +1,4 @@
-import mimetypes
+import std/[mimetypes, macros]
 import guildenstern/[httpserver]
 import scorper_v2/[accept_parser]
 
@@ -20,3 +20,49 @@ iterator acceptMimes*(): string =
         let ext = getExt(item.mime)
         if ext.len > 0:
           yield ext
+
+macro acceptMime*( body: untyped): untyped =
+  expectKind body,nnkStmtList
+  expectKind body[0],nnkCaseStmt
+  var matched = genSym(nskvar)
+  let ext = body[0][0]
+  expectKind ext, nnkIdent
+  let extInject = nnkPragmaExpr.newTree(
+      ext,
+      nnkPragma.newTree(
+        ident("inject")
+      )
+    )
+  expectKind body[0][^1],nnkElse
+  var stmts = copyNimTree(body)
+  var cases = stmts[0][1 .. ^2]
+  for n in mitems(cases):
+    if n.kind == nnkOfBranch:
+      n[^1].add nnkAsgn.newTree(matched, ident("true"))
+      n[^1].add nnkBreakStmt.newTree(newEmptyNode())
+  var newCases = nnkCaseStmt.newTree(ext)
+  for n in cases:
+    newCases.add n
+  result = nnkStmtList.newTree(
+    nnkVarSection.newTree(
+      nnkIdentDefs.newTree(
+        matched,
+        newEmptyNode(),
+        ident("false"),
+      )
+    ),
+    nnkForStmt.newTree(
+      extInject,
+      newCall(bindSym("acceptMimes")),
+      newCases
+    ),
+    nnkIfStmt.newTree(
+      nnkElifBranch.newTree(
+        nnkPrefix.newTree(
+          ident("not"),
+          matched
+        ),
+        body[0][^1][0]
+      )
+    )
+  )
